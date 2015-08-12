4823caa2adbba898158ca028e3e0791a1ab2bca3
bhbrantley <brittain.brantley@gmail.com>
2015-08-11 20:53:55 -0400

    Finalized player event view

--- PGA/Players.R
+++ PGA/Players.R
@@ -8,6 +8,9 @@
 library(rvest)
 library(XML)
 
+
+####  Player Extraction -------------------
+
 # Set source
 html <- html("http://www.pgatour.com/stats/stat.138.html")
 
@@ -16,34 +19,46 @@ player <- html_nodes(html, ".player-name") %>%
   html_text(player, trim = TRUE)
 head(player)
 length(player)
+# Convert to dataframe and remove unnecessary rows
+player <- as.data.frame(player)
+player <- player[-c(1,2), ]
 
 # Number of Total Events
 events <- html_nodes(html, ".hidden-small:nth-child(4)") %>%
-  html_text(events)
+  html_text(events) %>%
+  as.numeric()
 head(events)
 length(events)
+events <- as.data.frame(events)
 
 # Number of Top Ten finished
 stat.topten <- html_nodes(html, ".hidden-small:nth-child(5)") %>% 
-  html_text(stat.topten)
+  html_text(stat.topten) %>%
+  as.numeric()
 length(stat.topten)
+stat.topten <- as.data.frame(stat.topten)
 
 # Number of First place finishes
 stat.first <- html_nodes(html, ".hidden-small:nth-child(6)") %>%
-  html_text(stat.first)
+  html_text(stat.first) %>%
+  as.numeric()
 length(stat.first)
+stat.first <- as.data.frame(stat.first)
 
 # Number of Second place finishes
 stat.second <- html_nodes(html, ".hidden-small:nth-child(7)") %>% 
-  html_text(stat.second)
+  html_text(stat.second) %>%
+  as.numeric()
 length(stat.second)
-
+stat.second <- as.data.frame(stat.second)
 
 # Number of Third place finishes
 stat.third <- html_nodes(html, ".hidden-small:nth-child(8)") %>%
   html_text(stat.third) %>%
+  as.numeric()
 length(stat.third)
+stat.third <- as.data.frame(stat.third)
+
 
 ####  Create Data frame -------------------
-df <- as.data.frame(cbind(player, events, stat.topten, stat.first, stat.second, stat.third))
-
+df <- as.data.frame(cbind(player, events, stat.topten, stat.first, stat.second))
